32712ff227531a95614d2bc24fe0f669ba8f8157
Gagana004 <gaganakavindu96@gmail.com>
2021-02-23 09:03:51 +0530

    block chain

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
+const SHA256 = require('crypto-js/sha256');  //Encription Method
+
 class Block{
-    constructor(index, timestamp, data, previousHash = ""){
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
 
     }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
 }
+
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenisisBlock()]
+    }
+
+    createGenisisBlock(){
+        return new Block(0, "01-01-2021", "Genesis block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let dogeCoin = new Blockchain();
+dogeCoin.addBlock(new Block(1, "02-01-2021", {amount: 4}));
+dogeCoin.addBlock(new Block(2, "02-01-2021", {amount: 10}));
+
+console.log(JSON.stringify(dogeCoin, null, 4));
